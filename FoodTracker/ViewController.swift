fe5e382493321c0b3e39a2aae0aad252cce14dda
Justin Dang <dangjustintime@gmail.com>
2019-03-25 12:27:27 -0400

    left off at Connect the UI to code, Process User Input

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // MARK: actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default text"
+    }
+    
+    
 }
 
