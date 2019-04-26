aedce4af7e58101bab83860bcb16f0fb4ad02965
Justin Dang <dangjustintime@gmail.com>
2019-04-26 16:26:14 -0400

    left off at To implement the imagePickerControllerDidCancel method

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate,
+    UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // MARK: properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,10 +32,18 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     // MARK: actions
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // hide keyboard
+        nameTextField.resignFirstResponder()
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library
+        let imagePickerController = UIImagePickerController()
+        // allows photos to be picked, not taken
+        imagePickerController.sourceType = .photoLibrary
+        // make sure ViewController is notified when the user picks an image
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default text"
     }
-    
-    
 }
-
