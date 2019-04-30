9740fd300588fb3b64156c3d4d6aaefab961c6f1
Justin Dang <dangjustintime@gmail.com>
2019-04-30 12:14:31 -0400

    finished implemented edit and delete behavior

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -25,6 +25,13 @@ class MealViewController: UIViewController, UITextFieldDelegate,
         super.viewDidLoad()
         // handle the text view's user input through delegate callbacks
         nameTextField.delegate = self
+        // Set up views if editing an existing Meal.
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
         // Enable the Save button only if the text field has a valid Meal name.
         updateSaveButtonState()
     }
@@ -59,7 +66,15 @@ class MealViewController: UIViewController, UITextFieldDelegate,
     }
     // MARK: navigation
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        } else if let owningNavigationController = navigationController {
+            owningNavigationController.popViewController(animated: true)
+        } else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
     }
     // lets you configure a view controller before it's presented
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
