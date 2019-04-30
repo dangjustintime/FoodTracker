696e8054f133827c3cd40453d80cc16b549fffd1
Justin Dang <dangjustintime@gmail.com>
2019-04-30 12:56:15 -0400

    finished persist data

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSCoding {
     // MARK: properties
     var name: String
     var photo: UIImage?
     var rating: Int
+    // MARK: archiving paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+    // MARK: types
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
     // MARK: initialization
     init?(name: String, photo: UIImage?, rating: Int) {
         // name must not be empty
@@ -26,5 +36,22 @@ class Meal {
         self.name = name
         self.photo = photo
         self.rating = rating
+    }
+    // MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    required convenience init?(coder aDecoder: NSCoder) {
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
     }
 }
